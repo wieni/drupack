9e84c4cbaadd5522a8deedf014fd1d1285dcccaf
Kobe Aerts <kobe.aerts@gmail.com>
2016-11-26 15:43:50 +0100

    no image optimisations, do it only once!

--- webpack/prod.config.babel.js
+++ webpack/prod.config.babel.js
@@ -38,7 +38,7 @@ module.exports = {
                 test: /\.(jpe?g|png|gif|svg)$/i,
                 loaders: [
                     'file?hash=sha512&digest=hex&name=[hash].[ext]',
-                    'image-webpack?bypassOnDebug&optimizationLevel=7&interlaced'
+                    'image-webpack?bypassOnDebug&optimizationLevel=0'
                 ]
             }
         ]
