7c68c274c6126718d528c27968c12874dfff574e
Sander <sander@wieni.be>
2020-03-27 18:25:48 +0100

    Update setup

--- resources/index.js
+++ resources/index.js
@@ -1,5 +1,1 @@
-import React from "react";
-import { render } from "react-dom";
-import { Gallery } from "@base/components";
-
-render(<Gallery />, document.getElementById("app"));
+import "./index.scss";
