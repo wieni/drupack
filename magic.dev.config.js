e77e0877211ab1c57c8fe28bb66f33160ef78f83
Sander <sander@wieni.be>
2020-03-28 11:44:24 +0100

    Add new magic config

---
+++ magic.dev.config.js
@@ -0,0 +1,7 @@
+/* webpack-dev-server config */
+
+module.exports = {
+  proxyPort: 3000,
+  proxyTarget: "https://test.sander.wieni.dev",
+  proxyHost: "test.sander.wieni.dev",
+};
--- resources/index.js
+++ resources/index.js
@@ -1,1 +1,3 @@
 import "./index.scss";
+
+console.log('hello');
