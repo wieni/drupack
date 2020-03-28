a446ce8b184f3e3740336e5be9c1b20534019278
Sander <sander@wieni.be>
2020-03-28 16:41:24 +0100

    add hot-loader test

--- resources/index.js
+++ resources/index.js
@@ -1,6 +1,12 @@
+import "./index.scss";
+
+import "react-hot-loader";
 import React from "react";
 import { render } from "react-dom";
+import { hot } from "react-hot-loader/root";
 
-const App = () => <div>Hello world</div>;
+const App = () => <div>Hello there space!</div>;
 
-render(<App />, document.getElementById("app"));
+const HotApp = hot(App);
+
+render(<HotApp />, document.getElementById("app"));
