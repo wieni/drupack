3f8ba62fa35dd446fee2252825895123822d2050
Sander <sander@wieni.be>
2020-03-31 20:34:51 +0200

    Remove hot-loeader

--- resources/index.js
+++ resources/index.js
@@ -1,12 +1,6 @@
 import "./index.scss";
 
-import "react-hot-loader";
-import React from "react";
-import { render } from "react-dom";
-import { hot } from "react-hot-loader/root";
+import { render } from "@magic";
+import { Gallery } from "@bunny/components";
 
-const App = () => <div>Hello there space!</div>;
-
-const HotApp = hot(App);
-
-render(<HotApp />, document.getElementById("app"));
+render(Gallery, "app");
